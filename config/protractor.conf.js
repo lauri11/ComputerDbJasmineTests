4970f44d75c5ffed92d237748b59367978bae872
lauri11 <elena.korzhuk@gmail.com>
2017-03-28 13:47:45 +0300

    Added directConnect property to protractor.conf.js; modified AddComputerPage.js so that to use ES6 class syntax

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -1,19 +1,16 @@
 exports.config = {
+    directConnect: true,
+
     seleniumAddress: 'http://localhost:4444/wd/hub',
 
-    // Capabilities to be passed to the webdriver instance.
     capabilities: {
         'browserName': 'chrome'
     },
 
-    // Framework to use. Jasmine is recommended.
     framework: 'jasmine',
 
-    // Spec patterns are relative to the current working directory when
-    // protractor is called.
     specs: ['./../specs/*.js'],
 
-    // Options to be passed to Jasmine.
     jasmineNodeOpts: {
         defaultTimeoutInterval: 30000
     },
--- pages/AddComputerPage.js
+++ pages/AddComputerPage.js
@@ -1,137 +1,71 @@
 let SelectWrapper = require("./SelectWrapper.js");
 
-let AddComputerPage = function () {
-    this.pageHeading = element(by.css("#main h1"));
-    this.computerNameLabel = element(by.css("label[for='name']"));
-    this.computerNameInput = element(by.id("name"));
-    this.introducedDateLabel = element(by.css("label[for='introduced']"));
-    this.introducedDateInput = element(by.id("introduced"));
-    this.discontinuedDateLabel = element(by.css("label[for='discontinued']"));
-    this.discontinuedDateInput = element(by.id("discontinued"));
-    this.companySelectLabel = element(by.css("label[for='company']"));
-    this.companySelect = element(by.id("company"));
-    this.createComputerButton = element(by.css("input[value='Create this computer']"));
-    this.deleteComputerButton = element(by.css("input[value='Delete this computer']"));
-    this.cancelButton = element(by.xpath("//a[@class='btn'][text()='Cancel']"));
-    this.computerNameDiv = element(by.xpath("//label[@for='name']/.."));
-    let selectWrapper = new SelectWrapper(this.companySelect);
+class AddComputerPage {
 
-    this.setComputerName = function (computerName) {
+    constructor() {
+        this.pageHeading = element(by.css("#main h1"));
+        this.computerNameLabel = element(by.css("label[for='name']"));
+        this.computerNameInput = element(by.id("name"));
+        this.introducedDateLabel = element(by.css("label[for='introduced']"));
+        this.introducedDateInput = element(by.id("introduced"));
+        this.discontinuedDateLabel = element(by.css("label[for='discontinued']"));
+        this.discontinuedDateInput = element(by.id("discontinued"));
+        this.companySelectLabel = element(by.css("label[for='company']"));
+        this.companySelect = element(by.id("company"));
+        this.createComputerButton = element(by.css("input[value='Create this computer']"));
+        this.deleteComputerButton = element(by.css("input[value='Delete this computer']"));
+        this.cancelButton = element(by.xpath("//a[@class='btn'][text()='Cancel']"));
+        this.computerNameDiv = element(by.xpath("//label[@for='name']/.."));
+        this.selectWrapper = new SelectWrapper(this.companySelect);
+    }
+
+    setComputerName(computerName) {
         return this.computerNameInput.sendKeys(computerName);
-    };
+    }
 
-    this.setIntroducedDate = function (introducedDate) {
+    setIntroducedDate(introducedDate) {
         return this.introducedDateInput.sendKeys(introducedDate);
-    };
+    }
 
-    this.setDiscontinuedDate = function (discontinuedDate) {
+    setDiscontinuedDate(discontinuedDate) {
         return this.discontinuedDateInput.sendKeys(discontinuedDate);
-    };
+    }
 
-    this.selectCompany = function (companyName) {
-        return selectWrapper.selectItemByValue(companyName);
-    };
+    selectCompany(companyName) {
+        return this.selectWrapper.selectItemByValue(companyName);
+    }
 
-    this.submitNewComputerForm = function () {
+    submitNewComputerForm() {
         return this.createComputerButton.click();
-    };
+    }
 
-    this.deleteComputer = function () {
+    deleteComputer() {
         return this.deleteComputerButton.click();
-    };
+    }
 
-    this.addNewComputer = function (name, introducedDate, discontinuedDate, company) {
+    addNewComputer(name, introducedDate, discontinuedDate, company) {
         this.setComputerName(name);
         if (introducedDate) this.setIntroducedDate(introducedDate);
         if (discontinuedDate) this.setDiscontinuedDate(discontinuedDate);
         if (company) this.selectCompany(company);
         this.submitNewComputerForm()
-    };
+    }
 
-    this.clickCancelButton = function () {
+    clickCancelButton() {
         return this.cancelButton.click();
-    };
+    }
 
-    this.cancelCreatingComputer = function (name, introducedDate, discontinuedDate, company) {
+    cancelCreatingComputer(name, introducedDate, discontinuedDate, company) {
         this.setComputerName(name);
         if (introducedDate) this.setIntroducedDate(introducedDate);
         if (discontinuedDate) this.setDiscontinuedDate(discontinuedDate);
         if (company) this.selectCompany(company);
         this.clickCancelButton();
-    };
+    }
 
-    this.getComputerNameDivClass = function () {
+    getComputerNameDivClass() {
         return this.computerNameDiv.getAttribute("class");
-    };
-};
-
-
-// class AddComputerPage {
-//
-//     constructor() {
-//         this.pageHeading = element(by.css("#main h1"));
-//         this.computerNameLabel = element(by.css("label[for='name']"));
-//         this.computerNameInput = element(by.id("name"));
-//         this.introducedDateLabel = element(by.css("label[for='introduced']"));
-//         this.introducedDateInput = element(by.id("introduced"));
-//         this.discontinuedDateLabel = element(by.css("label[for='discontinued']"));
-//         this.discontinuedDateInput = element(by.id("discontinued"));
-//         this.companySelectLabel = element(by.css("label[for='company']"));
-//         this.companySelect = element(by.id("company"));
-//         this.createComputerButton = element(by.css("input[value='Create this computer']"));
-//         this.deleteComputerButton = element(by.css("input[value='Delete this computer']"));
-//         this.cancelButton = element(by.xpath("//a[@class='btn'][text()='Cancel']"));
-//         this.computerNameDiv = element(by.xpath("//label[@for='name']/.."));
-//         this.selectWrapper = new SelectWrapper(this.companySelect);
-//     }
-//
-//     setComputerName(computerName) {
-//         return this.computerNameInput.sendKeys(computerName);
-//     }
-//
-//     setIntroducedDate(introducedDate) {
-//         return this.introducedDateInput.sendKeys(introducedDate);
-//     }
-//
-//     setDiscontinuedDate(discontinuedDate) {
-//         return this.discontinuedDateInput.sendKeys(discontinuedDate);
-//     }
-//
-//     selectCompany(companyName) {
-//         return selectWrapper.selectItemByValue(companyName);
-//     }
-//
-//     submitNewComputerForm() {
-//         return this.createComputerButton.click();
-//     }
-//
-//     deleteComputer() {
-//         return this.deleteComputerButton.click();
-//     }
-//
-//     addNewComputer(name, introducedDate, discontinuedDate, company) {
-//         this.setComputerName(name);
-//         if (introducedDate) this.setIntroducedDate(introducedDate);
-//         if (discontinuedDate) this.setDiscontinuedDate(discontinuedDate);
-//         if (company) this.selectCompany(company);
-//         this.submitNewComputerForm()
-//     }
-//
-//     clickCancelButton() {
-//         return this.cancelButton.click();
-//     }
-//
-//     cancelCreatingComputer(name, introducedDate, discontinuedDate, company) {
-//         this.setComputerName(name);
-//         if (introducedDate) this.setIntroducedDate(introducedDate);
-//         if (discontinuedDate) this.setDiscontinuedDate(discontinuedDate);
-//         if (company) this.selectCompany(company);
-//         this.clickCancelButton();
-//     }
-//
-//     getComputerNameDivClass() {
-//         return this.computerNameDiv.getAttribute("class");
-//     }
-// }
+    }
+}
 
 module.exports = AddComputerPage;
